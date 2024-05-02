99e406001d2b5924806101eec997e1caf1b703a7
Kateryna Davidovych <kathylevchenko1@gmail.com>
2024-05-02 12:31:42 +0300

    add components

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,31 @@
+import css from "./TransactionHistory.module.css"
+export const TransactionHistory = ({ transactions }) => {
+    const transformedTransactions = transactions.map((transaction) => ({
+      ...transaction,
+      amount: parseFloat(transaction.amount),
+    }));
+    return (
+      <table className={css.table}>
+        <thead className={css.tableHead}>
+          <tr className={css.tableRow}>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+  
+        <tbody className={css.tableBody}>
+          {transformedTransactions.map((transaction) => (
+            <tr className={css.tableRow} key={transaction.id}>
+              <td className={css.tableDescription}>{transaction.type}</td>
+              <td className={css.tableDescription}>{transaction.amount}</td>
+              <td className={css.tableDescription}>{transaction.currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
+  
+  export default TransactionHistory;
